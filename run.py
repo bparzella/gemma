80e2c4e8d2f113d67fd0f74788d0d705145445e3
Benjamin Parzella <bparzella@googlemail.com>
2015-04-29 23:35:28 +0200

    added stack dump on sigusr1

--- run.py
+++ run.py
@@ -14,8 +14,35 @@
 # GNU Lesser General Public License for more details.
 #####################################################################
 
+import sys
+import signal
+import traceback
+import threading
+
 import app as appmodule
 from app import app
+
+def strackTrace(num, frame):
+	print >> sys.stderr, "\n*** STACKTRACE - START ***\n"
+
+	threadNames = {}
+	for thread in threading.enumerate():
+		threadNames[thread.ident] = thread.getName()
+
+	code = []
+	for threadId, stack in sys._current_frames().items():
+	    code.append("\n# ThreadName: %s ThreadID: %s" % (threadNames[threadId], threadId))
+	    for filename, lineno, name, line in traceback.extract_stack(stack):
+	        code.append('File: "%s", line %d, in %s' % (filename,
+	                                                    lineno, name))
+	        if line:
+	            code.append("  %s" % (line.strip()))
+
+	for line in code:
+	    print >> sys.stderr, line
+	print >> sys.stderr, "\n*** STACKTRACE - END ***\n"
+
+signal.signal(signal.SIGUSR1, strackTrace)
 
 app.run(host="0.0.0.0", port=4999, debug=True, use_reloader=False, threaded=True)
 
